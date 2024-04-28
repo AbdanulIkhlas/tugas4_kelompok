2143c4ccee94970254e0fa0eb8f603f9da3e4af4
KLaZ <muhabdanulikhlas0983@gmail.com>
2024-04-28 08:05:03 +0700

    install url launch

--- lib/favoritepage.dart
+++ lib/favoritepage.dart
@@ -1,28 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:tugas4_kelompok/data/site.dart';
 import 'package:tugas4_kelompok/sitemanager.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class FavoritePage extends StatefulWidget {
   const FavoritePage({Key? key});
 
   @override
-  State<StatefulWidget> createState() {
-    return _FavoritePageState();
-  }
+  State<FavoritePage> createState() => _FavoritePageState();
 }
 
 class _FavoritePageState extends State<FavoritePage> {
   final SiteManager _siteManager = SiteManager();
+  late List<int> _favoriteIndices = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadFavoriteIndicesFromSharedPreferences();
+  }
+
+  Future<void> _loadFavoriteIndicesFromSharedPreferences() async {
+    final prefs = await SharedPreferences.getInstance();
+    final favoriteIndices =
+        prefs.getStringList('favorite_sites')?.map((index) => int.parse(index)).toList() ?? [];
+    setState(() {
+      _favoriteIndices = favoriteIndices;
+    });
+  }
+
+  // Function to launch website link in a new tab
+  void _launchLink(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url, forceWebView: false, enableJavaScript: true);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  // Function to show notification when site is favorited or unfavorited
+  void _showNotification(bool favorited) {
+    String message = favorited ? 'Site Favorited' : 'Site Unfavorited';
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        duration: Duration(seconds: 1),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    List<int> favoriteIndices = _siteManager.getFavoriteSites();
-
     return Scaffold(
       appBar: AppBar(
+        leading: BackButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
         title: Text(
-          'Favorite Sites',
-          style: TextStyle(color: Colors.white),
+          "Daftar Favorit",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
         backgroundColor: Colors.teal,
       ),
@@ -31,21 +70,21 @@ class _FavoritePageState extends State<FavoritePage> {
           gradient: LinearGradient(
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
-            colors: [Colors.teal.withOpacity(0.8), Colors.white],
+            colors: [Colors.teal, Colors.white],
           ),
         ),
         child: ListView.builder(
-          itemCount: favoriteIndices.length,
+          itemCount: _favoriteIndices.length,
           itemBuilder: (BuildContext context, int index) {
-            final favoriteIndex = favoriteIndices[index];
+            final favoriteIndex = _favoriteIndices[index];
             final site = GenerateSite.getDataSites()[favoriteIndex];
             return Card(
-              elevation: 3,
+              elevation: 5,
               margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15),
-              ),
               child: ListTile(
+                onTap: () {
+                  _launchLink(site.url); // Launch link when ListTile is tapped
+                },
                 leading: Image(
                   image: AssetImage(site.image),
                   width: 50,
@@ -54,9 +93,21 @@ class _FavoritePageState extends State<FavoritePage> {
                 subtitle: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(site.url),
+                    Text(
+                      site.url,
+                      style: TextStyle(color: Colors.blue), // Style link text
+                    ),
                     Text(site.description),
                   ],
+                ),
+                trailing: IconButton(
+                  icon: Icon(Icons.star),
+                  onPressed: () {
+                    setState(() {
+                      _siteManager.toggleFavorite(favoriteIndex);
+                      _showNotification(_siteManager.favoriteIndices.contains(favoriteIndex));
+                    });
+                  },
                 ),
               ),
             );
